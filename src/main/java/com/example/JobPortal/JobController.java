f24d2636b048262f548440ea4a9ccfcfd4104f37
Dhanush B <dhanushcse12@gmail.com>
2024-06-09 17:19:16 +0530

    First Spring/SpringBoot MVC APP

---
+++ src/main/java/com/example/JobPortal/JobController.java
@@ -0,0 +1,44 @@
+package com.example.JobPortal;
+
+import com.example.JobPortal.model.JobPost;
+import com.example.JobPortal.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class JobController {
+
+    @Autowired
+    private JobService service;
+
+    @RequestMapping({"/","/home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/addjob")
+    public String addJob() {
+        return "addjob";
+    }
+
+    @GetMapping("/viewalljobs")
+    public String viewJobs(Model model) {
+        List<JobPost> jobPosts = service.returnAllJobPosts();
+        model.addAttribute("jobPosts", jobPosts);
+        return "viewalljobs";
+    }
+
+    @PostMapping("/handleForm")
+    public String handleForm(JobPost jobPost, Model model){
+        model.addAttribute("jobPost", jobPost);
+        service.addJobPost(jobPost);
+        return "success";
+    }
+
+}
